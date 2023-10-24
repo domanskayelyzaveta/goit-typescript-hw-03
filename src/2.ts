9d0636562e2e7887394a4944958b8300997f536f
domanskayelyzaveta <domanskaya_@ukr.net>
2023-10-24 20:11:27 +0200

    hw num 3 created

--- src/2.ts
+++ src/2.ts
@@ -15,25 +15,21 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
-
-  getEmployeeDetails() {
+  constructor(public name: string, private department: string, protected salary: number) { }
+   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
   }
 }
 
+// Реалізуйте конструктор та збільшіть salary на 10000
+
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+    this.salary = salary + 1000;
+  }
 }
 
 
-export {};
+export { };
+
